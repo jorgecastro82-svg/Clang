2deb579947c3ec32fd848378304e2d953c0ef1db
jorgecastro82-svg <jorge.castro82@uabc.edu.mx>
2025-10-25 19:26:33 +0000

    juego.c y libreria

--- TicTacNegro/juego.c
+++ TicTacNegro/juego.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+void printfRojo(char*);
+
+void printfAzul(char*);
+
 int main(){
 
-    printf("Hola negros");
+    printfRojo("Hola negros\n");
+    printfAzul("Hola negros\n");
+
     return 0;
+}
+
+void printfRojo(char *cadena){
+    printf("\033[1;31m%s\033[0m",cadena);
+}
+
+void printfAzul(char *cadena){
+    printf("\033[1;34m%s\033[0m",cadena);
 }
