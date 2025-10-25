a8832a993d72ba564303200fb7d9399e428c7ddf
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-10-25 22:22:23 +0000

    Camarones

--- TicTacNegro/juego.c
+++ TicTacNegro/juego.c
@@ -4,7 +4,7 @@
 
 #define COL 11
 
-int menu(char *,int*,int*,int*,int*,int*,int*);
+int menu(char *,int*,int*,int*,int*,int*,int*,int*);
 
 void Imptablero(char *);
 
@@ -14,9 +14,9 @@ void Imagenrespuestamediana(char *);
 
 void Imagenrespuestachica(char *);
 
-void Imagenrespuestagrande(char *);
+void Imagenrespuestagrande(char *,int*);
 
-void tamano(char*,int*,int*,int*,int*,int*,int*);
+void tamano(char*,int*,int*,int*,int*,int*,int*,int*);
 
 void printfRojoChar(char);
 
@@ -29,10 +29,11 @@ int main(){
 	int grandeT2=0;
 	int chicoT2=0;
 	int medT2=0;
+	int player=1;
     char respuesta[REN][COL];
     int opc=0;
     do{
-        opc=menu((char*)respuesta,&grandeT1,&grandeT2,&medT1,&medT2,&chicoT1,&chicoT2);
+        opc=menu((char*)respuesta,&grandeT1,&grandeT2,&medT1,&medT2,&chicoT1,&chicoT2,&player);
     }while(opc==0);
 	printf("\n");
     return 0;
@@ -98,7 +99,7 @@ void Imagenrespuestachica(char *matriz){
 			}
 			}
 }
-void Imagenrespuestagrande(char *matriz){
+void Imagenrespuestagrande(char *matriz,int* g){
 	int i,j;
 	for(i=0;i<(REN)-(8);i++){
 		for(j=0;j<(COL)-(8);j++){
@@ -108,16 +109,23 @@ void Imagenrespuestagrande(char *matriz){
 
 		}
 	}
+	(*g)++;
 }
-int menu(char* respuesta,int*g1,int*g2,int*m1,int*m2,int*c1,int*c2){
+int menu(char* respuesta,int*g1,int*g2,int*m1,int*m2,int*c1,int*c2,int*player){
     Inicirespuesta(respuesta);
-	tamano(respuesta,g1,g2,m1,m2,c1,c2);
+	tamano(respuesta,g1,g2,m1,m2,c1,c2,player);
 	Imptablero(respuesta);
-	
+	(*player)++;
+	printf("\n%d",(*player));
+	printf("\n%d",(*g1));
+	printf("\n%d",(*g2));
+	if((*g1)==2||(*g2)==2){
     return 1;
+	}
+	return 0;
 }
 
-void tamano(char* respuesta,int* g1,int* g2,int* m1,int* m2,int* c1,int* c2){
+void tamano(char* respuesta,int* g1,int* g2,int* m1,int* m2,int* c1,int* c2,int* p){
 	int pz=0;
 	printf("Escoga el tamano de la pieza\n");
 	printf("[1]Grande\n");
@@ -126,7 +134,22 @@ void tamano(char* respuesta,int* g1,int* g2,int* m1,int* m2,int* c1,int* c2){
 	scanf("%d",&pz);
 	switch(pz){
 		case 1:
-		Imagenrespuestagrande(respuesta);
+			if(((*p)%2)!=0){
+				if((*g1)<2){
+					Imagenrespuestagrande(respuesta,g1);
+				}
+				else{
+					printf("Se te acabaron las piezas grandes");
+				}
+			}
+			 if(((*p)%2)==0){
+				if((*g2)<2){
+					Imagenrespuestagrande(respuesta,g2);
+				}
+				else{
+					printf("Se te acabaron las piezas grandes");
+				}
+			}
 		break;
 		case 2:
 		Imagenrespuestamediana(respuesta);
